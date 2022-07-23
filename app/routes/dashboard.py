72856c551a2a1bcdd4ce203dbc36fc091f6dbb27
amarten <alexdmarten@gmail.com>
2022-07-22 19:33:27 -0700

    added some comments to explain routes

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -2,10 +2,12 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
+# route for the home dashboard page
 @bp.route('/')
 def dash():
     return render_template('dashboard.html')
 
+# route for user to edit a post
 @bp.route('edit/<id>')
 def edit(id):
     return render_template('edit-post.html')
